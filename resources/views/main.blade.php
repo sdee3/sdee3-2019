5ce9b57bd700e885a089135183c0368d744b0dc2
Stefan <stefandjokic@pm.me>
2019-07-21 16:17:36 +0200

    Updated favicon

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
-  <link rel="icon" href="https://cdn.iconscout.com/icon/free/png-256/laravel-226015.png" sizes="32x32">
+  <link rel="icon" href="favicon.ico">
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
     integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
